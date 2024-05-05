9d40ee6fb70e228d2ec47c723e0f6e4dab88eb22
Ludc08 <126533991+Ludc08@users.noreply.github.com>
2024-05-05 15:08:45 +0800

    Update api.py

--- api/api.py
+++ api/api.py
@@ -1,8 +1,17 @@
 import requests
-URL = 'https://httpbin.org/'
+URL = 'https://www.flickr.com/services/rest/'
+Key = u'87276a62d22f7ff7ed01e971436a685f'
+Secret = '3c0a5c7e7645b84d'
 #param = 'Следующее утверждение верно. Предыдущее утверждение ложно'
-response = requests.get(f'{URL}/status/418')
-#response = requests.get(URL + 'image', headers={"accept": "image/png"})
-#with open('image.png', 'wb') as f:
-#    f.write(response.content)
-print(response.text)
+# response = requests.get(f'{URL}/status/418')
+method = '?method=flickr.auth.oauth.getAccessToken'
+arguments = '&api_key={Secret}:{Key}'
+# response = requests.get(URL + 'image', headers={"accept": "image/svg+xml"})
+# with open('image.svg+xml', 'wb') as f:
+#     f.write(response.content)
+
+response = requests.get(URL + method + arguments)
+print(response)
+print(response.headers)
+print(response.content)
+print(response.json())
