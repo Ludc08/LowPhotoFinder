5b8f4b663c4464dc6504b16e122687dc807b5182
Ludc08 <126533991+Ludc08@users.noreply.github.com>
2024-05-19 16:38:17 +0800

    Halleluah api.py

--- api/api.py
+++ api/api.py
@@ -1,17 +1,20 @@
+import xml.etree.ElementTree as ET
 import requests
 URL = 'https://www.flickr.com/services/rest/'
-Key = u'87276a62d22f7ff7ed01e971436a685f'
+key = u'87276a62d22f7ff7ed01e971436a685f'
 Secret = '3c0a5c7e7645b84d'
-#param = 'Следующее утверждение верно. Предыдущее утверждение ложно'
-# response = requests.get(f'{URL}/status/418')
-method = '?method=flickr.auth.oauth.getAccessToken'
-arguments = '&api_key={Secret}:{Key}'
-# response = requests.get(URL + 'image', headers={"accept": "image/svg+xml"})
-# with open('image.svg+xml', 'wb') as f:
-#     f.write(response.content)
+method_query = '?method='
+method = 'flickr.photos.getRecent' 
+params = {'api_key': key}
+response = requests.get(URL + method_query + method, params=params)
 
-response = requests.get(URL + method + arguments)
-print(response)
-print(response.headers)
-print(response.content)
-print(response.json())
+tree = ET.ElementTree(ET.fromstring(response.content))
+count = 0
+root = tree.getroot()
+for child in root.iter('photo'):
+    url=f'https://live.staticflickr.com/{child.attrib['server']}/{child.attrib['id']}_{child.attrib['secret']}_b.jpg'
+    response = requests.get(url, stream = True)
+    with open(f'pict{count}.jpg', 'wb') as f:
+        for chunk in response.iter_content(1024):
+            f.write(chunk)
+    count += 1
